c77e44686f79e06abb3eec2033216ad9c86228e3
Ibraim Barrios <barrios.party@hotmail.com>
2026-06-02 11:11:08 -0600

    Crear datos semillas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory()->create(['email' => 'i@test.com']);
+        User::factory(19)->hasPosts(4)->create(); //hasPosts permite crear cuatro publicaciones por cada usuario
     }
 }
