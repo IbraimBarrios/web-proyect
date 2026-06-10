909e6148b4c6de9bdddaf931be80fa5fea9d13e4
Ibraim Barrios <barrios.party@hotmail.com>
2026-06-10 11:47:08 -0600

    Pagina central

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,10 +3,7 @@
         <form action="{{ route('friends.store', $user) }}" class="px-4 my-8" method="POST">
             @csrf
 
-            <input type="submit" 
-            class="px-4 py-2 bg-yellow-400 text-gray-800 font-semibold sm:rounded-lg text-xs"
-            value="Add Friends"
-            >
+            <x-submit-button>Add Friend</x-submit-button>
         </form>
         @foreach ($posts as $post)
             <x-card class="mb-4">
